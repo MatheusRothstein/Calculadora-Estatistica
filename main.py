9a7d3a4bfa29ca57a66bf274291ce493b362b58f
LLBueno <leandro.bueno@fatec.sp.gov.br>
2019-12-08 22:14:53 -0300

    Arrumada mediana e adicionado começo da distribuição por classe

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ class CalculadoraEstatistica:
     def is_impar(self, num):
         if (num % 2) != 0:
             return True
-        return False 
-    
+        return False
+
     def calculaMedia(self):
         return sum(self.listaValores) / len(self.listaValores)
 
@@ -20,8 +20,9 @@ class CalculadoraEstatistica:
         if self.is_impar(len(numeros_ordenados)):
             meio = int(len(numeros_ordenados) / 2)
             return numeros_ordenados[meio]
-        meio_cima = int(round(len(numeros_ordenados) + 0.5))
-        meio_baixo = int(round(len(numeros_ordenados) - 0.5))
+        import pdb; pdb.set_trace()
+        meio_cima = int(round(len(numeros_ordenados) / 2 + 0.5))
+        meio_baixo = int(round(len(numeros_ordenados) / 2))
         return (numeros_ordenados[meio_cima] + numeros_ordenados[meio_baixo]) / 2
 
     def calculaModa(self):
@@ -45,24 +46,30 @@ class CalculadoraEstatistica:
         var = 0
         variance = 0
         for item in self.listaValores:
-            var += ((item - media) ** 2) 
+            var += ((item - media) ** 2)
         variance = var / (len(self.listaValores) - 1)
         return variance
 
     def calculaDesvioPadrao(self):
         return sqrt(self.calculaVariancia())
 
+    def divisao_classes(self):
+        qtd_classes = int(round(sqrt(len(self.listaValores)) + 0.5))
+        amplitude = (max(self.listaValores) - max(self.listaValores)) // qtd_classes
+        return (qtd_classes, amplitude)
+
 
 if __name__ == "__main__":
-    valores_aleatorios = [randint(1, 10) for x in range(1, 10)]
+    valores_aleatorios = [float(randint(1, 10)) for x in range(1, 11)]
     calculo = CalculadoraEstatistica(listaValores=valores_aleatorios)
     print("nossa media", calculo.calculaMedia())
     print("statics", statistics.mean(valores_aleatorios))
     print("nossa mediana", calculo.calculaMediana())
-    print("statistics mediana",  statistics.median(valores_aleatorios))
+    print("statistics mediana", statistics.median(valores_aleatorios))
     print("nossa moda", calculo.calculaModa())
     print("statitics moda", statistics.mode(valores_aleatorios))
     print("nossa variancia", calculo.calculaVariancia())
     print("statistics variancia", statistics.variance(valores_aleatorios))
     print("nosso desvio padrao", calculo.calculaDesvioPadrao())
     print("statistics desvio padrao", statistics.stdev(valores_aleatorios))
+    print("DIVISÃO POR CLASSE", calculo.divisao_classes())
