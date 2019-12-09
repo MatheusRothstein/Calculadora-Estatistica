453f731d9340b8ca1b88b9feb60f0708e1f21e21
Matheus <matheus.vieira12@fatec.sp.gov.br>
2019-12-09 00:05:18 -0300

    Botões funcionando e adição da divisão de classes

--- mainWindow.py
+++ mainWindow.py
@@ -7,7 +7,6 @@ from PyQt5.QtWidgets import *
 from main import Calculadora as c
 
 
-
 class Calc(QMainWindow):
 
     def __init__(self):
@@ -17,44 +16,58 @@ class Calc(QMainWindow):
     def initUI(self):
         self.line = QLineEdit(self)
         self.line.move(5, 5)
-        self.line.setAlignment(Qt.AlignRight)
+        self.line.setAlignment(Qt.AlignLeft)
+        self.line.resize(390, 100)
         font = self.line.font()
         font.setPointSize(12)
         self.line.setFont(font)
         self.line.text()
 
+        self.answer = QTextEdit(self)
+        self.answer.setReadOnly(True)
+        self.answer.move(5, 350)
+        self.answer.resize(390, 160)
+
+        self.myLabel = QLabel(self)
+        self.myLabel.setText("Resultado: ")
+        self.myLabel.move(5, 325)
+        self.myLabel.setFont(font)
+
         zero = QPushButton("moda", self)
-        zero.move(5, 265)
+        zero.move(5, 285)
         zero.clicked.connect(self.calcularAModa)
         
         one = QPushButton("media", self)
-        one.move(5, 215)
+        one.move(5, 235)
         one.clicked.connect(self.calcularAMedia)
 
         two = QPushButton("mediana", self)
-        two.move(90, 215)
+        two.move(155, 235)
         two.clicked.connect(self.calcularAMediana)
 
         three = QPushButton("variância", self)
-        three.move(145, 215)
+        three.move(300, 235)
+        three.clicked.connect(self.calcularAVariancia)
 
         four = QPushButton("desvio padrão", self)
-        four.move(5, 165)
+        four.move(5, 185)
+        four.clicked.connect(self.calcularDesvioPadrao)
 
-        five = QPushButton("5", self)
-        five.move(60, 165)
+        five = QPushButton("Divisão de classes", self)
+        five.move(155, 185)
+        five.clicked.connect(self.calculaDivisaoDeClasses)
 
         six = QPushButton("6", self)
-        six.move(115, 165)
+        six.move(300, 185)
 
         seven = QPushButton("7", self)
-        seven.move(5, 115)
+        seven.move(5, 135)
 
         eight = QPushButton("8", self)
-        eight.move(60, 115)
+        eight.move(155, 135)
 
         nine = QPushButton("9", self)
-        nine.move(115, 115)
+        nine.move(300, 135)
 
 
         self.setGeometry(500, 500, 400, 400)
@@ -71,22 +84,39 @@ class Calc(QMainWindow):
     def calcularAModa(self):
         lista = self.getTextAndReturnList()
         self.function = c(lista)
-        self.function.calculaModa()
+        result = self.function.calculaModa()
+        self.answer.setText(str(result))
 
     def calcularAMedia(self):
         lista = self.getTextAndReturnList()
         self.function = c(lista)
-        self.function.calculaMedia()
+        resultado = self.function.calculaMedia()
+        self.answer.setText(str(resultado))
 
     def calcularAMediana(self):
         lista = self.getTextAndReturnList()
         self.function = c(lista)
-        self.function.calculaMediana()
+        resultado = self.function.calculaMediana()
+        self.answer.setText(str(resultado))
 
     def calcularAVariancia(self):
-        pass
+        lista = self.getTextAndReturnList()
+        self.function = c(lista)
+        resultado = self.function.calculaVariancia()
+        self.answer.setText(str(resultado))
 
-        
+    def calcularDesvioPadrao(self):
+        lista = self.getTextAndReturnList()
+        self.function = c(lista)
+        resultado = self.function.calculaDesvioPadrao()
+        self.answer.setText(str(int(resultado)))
+
+    def calculaDivisaoDeClasses(self):
+        lista = self.getTextAndReturnList()
+        self.function = c(lista)
+        resultado = self.function.formata_divisao_classes()
+        self.answer.setText(resultado)
+
 
 
 def main():
